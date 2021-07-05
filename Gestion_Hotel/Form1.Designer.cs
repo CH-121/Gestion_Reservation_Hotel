8255db9b12bb398d1b62c5ba9330906b25a519cf
CH-121 <cherkaouirabab@gmail.com>
2021-07-05 03:05:54 +0100

    initial commit

---
+++ Gestion_Hotel/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace Gestion_Hotel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ucHotel1 = new Gestion_Hotel.UC.ucHotel();
+            this.SuspendLayout();
+            // 
+            // ucHotel1
+            // 
+            this.ucHotel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.ucHotel1.Location = new System.Drawing.Point(12, 2);
+            this.ucHotel1.Name = "ucHotel1";
+            this.ucHotel1.Size = new System.Drawing.Size(705, 493);
+            this.ucHotel1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(734, 494);
+            this.Controls.Add(this.ucHotel1);
+            this.Name = "Form1";
+            this.Text = "Hotel";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private UC.ucHotel ucHotel1;
+    }
+}
+
---
+++ Gestion_Hotel/UC/ucHotel.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Gestion_Hotel.UC
+{
+    partial class ucHotel
+    {
+        /// <summary> 
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur de composants
+
+        /// <summary> 
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas 
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.lblNomHotel = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel
+            // 
+            this.flowLayoutPanel.BackColor = System.Drawing.Color.Moccasin;
+            this.flowLayoutPanel.Location = new System.Drawing.Point(240, 99);
+            this.flowLayoutPanel.Name = "flowLayoutPanel";
+            this.flowLayoutPanel.Size = new System.Drawing.Size(448, 342);
+            this.flowLayoutPanel.TabIndex = 0;
+            // 
+            // lblNomHotel
+            // 
+            this.lblNomHotel.AutoSize = true;
+            this.lblNomHotel.Font = new System.Drawing.Font("Corsiva", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNomHotel.ForeColor = System.Drawing.Color.Moccasin;
+            this.lblNomHotel.Location = new System.Drawing.Point(328, 28);
+            this.lblNomHotel.Name = "lblNomHotel";
+            this.lblNomHotel.Size = new System.Drawing.Size(0, 59);
+            this.lblNomHotel.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Gestion_Hotel.Properties.Resources.MyDoor;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 179);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(224, 293);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // ucHotel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblNomHotel);
+            this.Controls.Add(this.flowLayoutPanel);
+            this.Name = "ucHotel";
+            this.Size = new System.Drawing.Size(705, 493);
+            this.Load += new System.EventHandler(this.ucHotel_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel;
+        private System.Windows.Forms.Label lblNomHotel;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
